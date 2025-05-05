29f036b32c3cee1df88b52b29ea830967502d992
tsuno08 <nozomutsu.0806@gmail.com>
2025-05-05 10:14:08 +0900

    chore: TextIntentモジュールの全ファイルを削除

--- modules/text-intent/index.ts
+++
@@ -1,3 +0,0 @@
-// Reexport the native module. On web, it will be resolved to TextIntentModule.web.ts
-// and on native platforms to TextIntentModule.ts
-export { TextIntentModule } from './src/TextIntentModule';
--- modules/text-intent/src/TextIntentModule.ts
+++
@@ -1,8 +0,0 @@
-import { NativeModule, requireNativeModule } from 'expo';
-
-declare class TextIntentModuleClass extends NativeModule {
-  hello(): string;
-}
-
-// This call loads the native module object from the JSI.
-export const TextIntentModule = requireNativeModule<TextIntentModuleClass>('TextIntent');
